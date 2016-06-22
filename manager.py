31124f57b3d8006ba529e5aeb4901f332395cfc4
Sean Lewis <sean@splewis.net>
2016-06-22 15:48:09 -0700

    Add inital db migration data.

--- manager.py
+++ manager.py
@@ -1,12 +1,15 @@
-#! /usr/bin/env python2.7
+#!/usr/bin/env python2.7
 
 import get5
+from get5 import db
+
+from get5.models import User, Team, GameServer, Match
+import get5.models
+
 import flask_script
 import flask_migrate
 
-
 manager = flask_script.Manager(get5.app)
-
 
 # Database migrations
 migrate = flask_migrate.Migrate(get5.app, get5.db)
